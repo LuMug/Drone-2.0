56887d9768ffe248c13c4819aed96d545e5f3a89
giannigrasso <gianni.grasso@samtrevano.ch>
2021-10-07 16:28:09 +0200

    Refactory classe Drone
    Iniziata la ristrutturazione della classe Drone.

--- src/Drone2.0/src/drone/Drone.java
+++ src/Drone2.0/src/drone/Drone.java
@@ -1,42 +1,81 @@
 package drone;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+import java.net.SocketException;
+
 /**
- *
- * @author Alessandro Aloise
+ *The drone class represents the abstract class of a drone
+ *and is responsible for sending information from the client to it.
+ * 
+ * @author Alessandro Aloise, Gianni Grasso
  * @version 07.10.2021
  */
 public class Drone extends Thread {
     
     /**
-     * The value of ip.
+     * The ip address of the drone.
      */
-    protected final String ip = "192.168.10.1";
+    protected static final String DRONE_IP = "192.168.10.1";
 
     /**
-     * The value of portD.
+     * The listening port of the drone commands.
      */
-    protected final int portD = 8889;
-    
+    protected static final int COMMANDS_PORT = 8889;
     
     
     /**
-     * Get the value of ip.
-     *
-     * @return the value of ip.
+     * The client's communication port.
      */
-    public String getIp() {
-        return ip;
-    }
+    protected int clientPort;
     
-
     /**
-     * Get the value of portD
-     *
-     * @return the value of portD
+     * The socket for the communication of the drone commands
      */
-    public int getPortD() {
-        return portD;
+    protected DatagramSocket socket;
+    
+    /**
+     * The data packet sent to the drone.
+     */
+    protected DatagramPacket packet;
+    
+    /**
+     * The message received from the drone.
+     */
+    private String messageReceived;
+    
+    
+    /**
+     * Metodo costruttore personalizzato con 1 parametro.
+     */
+    public Drone() {
+        try {
+            socket = new DatagramSocket();
+        } catch (SocketException ex) {
+            System.out.println("ERRORE: " + ex.getMessage());
+        }
+        clientPort = socket.getLocalPort();
+        
+        //status.start();
     }
     
     
+    /**
+     * Send a command to the drone.
+     */
+    public void sendCommand(String messageToSend) {
+        try {
+            byte[] data = messageToSend.getBytes();
+            DatagramPacket packet = new DatagramPacket(data, data.length, InetAddress.getByName(DRONE_IP), COMMANDS_PORT);
+            //Riprendere da qui
+            
+            socket.send(packet);
+        } catch (SocketException ex) {
+            System.out.println("ERRORE: " + ex.getMessage());
+        } catch (IOException ex) {
+            System.out.println("ERRORE: " + ex.getMessage());
+        }
+    }
 }
