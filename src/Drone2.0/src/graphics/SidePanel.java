eb25db79426b9f7141afac151eb369e4a6270fe9
Michea Colautti <michea.colautti@samtrevano.ch>
2021-10-07 14:32:00 +0200

    Aggiunte icone GUI e Codice per grafica
    Ho aggiunto le icone della GUI (disegni drone + altimetro) e implementato il codice per la grafica.

---
+++ src/Drone2.0/src/graphics/SidePanel.java
@@ -0,0 +1,50 @@
+package graphics;
+
+import java.awt.Image;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Michea Colautti
+ * @version 07.10.2021
+ */
+public class SidePanel extends Model{
+
+    /**
+     * Costruttore della classe. Permette di istanziare l'immagine.
+     */
+    public SidePanel() {
+        //aggiunto riferimento a bin in class path e path di libreria
+        ImageIcon icon;
+        icon = new ImageIcon(getClass().getClassLoader().getResource("DroneLaterale.png"));
+        Image image = icon.getImage();
+        imageBig=toBufferedImage(image);
+    }
+
+    /**
+     * Metodo per il movimento dell'immagine. Aggiorna il valore
+     * dell'inclinazione, verificando che i valori registrati dal drone non
+     * superino l'inclinazione massima permessa dalla costante
+     * ImageModel.MAXDEG.
+     *
+     * @param rotate è l'inclinazione in gradi.
+     */
+    public void moving(int rotate) {
+        if (rotDeg < 0) {
+
+            if (rotate > -MAXDEG) {
+                rotDeg = rotate;
+                validate();
+                repaint();
+            }
+        } else {
+
+            if (rotate < MAXDEG) {
+                rotDeg = rotate;
+                validate();
+                repaint();
+            }
+        }
+    }
+}
+
