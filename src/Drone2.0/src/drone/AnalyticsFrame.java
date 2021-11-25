95a6206bce30e0140b35448bf1615d6d0b33fd06
Michea Colautti <michea.colautti@samtrevano.ch>
2021-11-25 19:04:45 +0100

    Update AnalyticsFrame.java
    Aggiunto ciclo foreach per statistiche.

--- src/Drone2.0/src/drone/AnalyticsFrame.java
+++ src/Drone2.0/src/drone/AnalyticsFrame.java
@@ -1,6 +1,5 @@
 package drone;
 
-
 import java.awt.GridLayout;
 import java.util.Queue;
 import javax.swing.JFrame;
@@ -8,43 +7,53 @@ import javax.swing.JLabel;
 
 /**
  *
- * @author Michea Colautti 
+ * @author Michea Colautti
  * @version 25/11/2021
-
+ *
  */
 public class AnalyticsFrame extends JFrame implements Runnable {
 
-    
     /**
      * Queue for history.
      */
     private static volatile Queue<String> analyticsBufferData;
-    
-    
-   public void setAnalyticsBufferData(Queue<String> analyticsBufferData) {
+
+    public void setAnalyticsBufferData(Queue<String> analyticsBufferData) {
         this.analyticsBufferData = analyticsBufferData;
+
     }
-   
 
     private JLabel lab;
-    
-    public AnalyticsFrame(){
-        this.setLayout(new GridLayout(1,2));
-        lab= new JLabel();
+
+    public AnalyticsFrame() {
+        this.setLayout(new GridLayout(1, 2));
+        this.setSize(200, 200);
+        lab = new JLabel();
         this.add(lab);
-        
-    
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
-    
-    
+
     @Override
     public void run() {
-        while(true){
-            lab.setText(analyticsBufferData.poll());
+        StringBuilder datas = new StringBuilder();
+        while (true) {
+            if (analyticsBufferData.size() > 0) {
+                for (String data : analyticsBufferData) {
+                    System.out.println(data);
+                    System.out.println(analyticsBufferData.poll());
+                    //datas.append(analyticsBufferData.poll());
+
+                    try {
+                        Thread.sleep(1000);
+                        //lab.setText(datas.toString());
+                    } catch (InterruptedException ex) {
+                    }
+
+                }
+            }
+
         }
-        
+
     }
-    
-     
 
 }
