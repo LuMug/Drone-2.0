de16d3566afe35fa6fb6a1dd2f067b93966a20ff
Michea Colautti <michea.colautti@samtrevano.ch>
2021-12-02 16:08:29 +0100

    Update RunLiveMac.sh
    rimossi percorsi assoluti da script di run

--- src/Drone2.0/Live/Script/RunLiveMac.sh
+++ src/Drone2.0/Live/Script/RunLiveMac.sh
@@ -4,4 +4,4 @@ script_dir=$(dirname "$0")
 cd $script_dir
 cd ../Tello-live-Nodejs
 chmod +x index.js
-/usr/local/bin/node /Users/micheacolautti/Documents/GitHub/Drone-2.0/src/Drone2.0/Live/Tello-live-Nodejs/index.js 
+node index.js 
